6bfc95fb171e71edcf3bb0901ad31b939d611f03
theThirdTiwi <youdertubmail@gmail.com>
2023-11-15 20:31:27 +0100

    add shit_scene with script to main scene and transform the position

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node3D
 
+var ship1 = preload("res://test_ship_01.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,5 +14,11 @@ func _process(delta):
 
 func _on_button_pressed():
 	print("start pressed")
+	
 	$Menu.hide()
+	var instance = ship1.instantiate()
+	instance.set_position(instance.get_position() + Vector3(-20, 0,0))
+	#instance.set_name("ship")
+	add_child(instance)
+	
 
