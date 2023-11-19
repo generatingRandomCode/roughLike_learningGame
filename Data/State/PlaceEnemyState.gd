6e14e16890addf370bf3338dcc17827eb2028f92
theThirdTiwi <youdertubmail@gmail.com>
2023-11-19 21:52:44 +0100

    spawn random enemy ship

--- Data/State/PlaceEnemyState.gd
+++ Data/State/PlaceEnemyState.gd
@@ -12,12 +12,32 @@ func enter(parameter := {}) -> void:
 	placeEnemy()
 	
 func placeEnemy():
+	
+	var shipList = []
+	var path = "res://Ships"	
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var fileName = dir.get_next()
+		while fileName != "":
+			if dir.current_is_dir():
+				continue
+			else:
+				shipList.append(fileName)
+			fileName = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+		
+	#print("fileNameName: ", shipList)
+	#	choose a random ship to spwan
+	var randShip = randi() % shipList.size()
 	# Get a random index to select a random child
 	var random_index = randi() % enemyGrid.get_child_count()
 	# Create a new node as a child
 	var shipInstance = shipTemplate.instantiate()
 	shipInstance.add_to_group("enemy")
-	shipInstance.build("Typ1")
+	
+	shipInstance.build(shipList[randShip].split(".")[0])
 	# Add the new node as a child to the random child
 	enemyGrid.get_child(random_index).add_child(shipInstance)
 	print("Added a new node to a random child.")
