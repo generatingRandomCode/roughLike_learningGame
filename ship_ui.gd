c5457ac37a79892222ce89c17fb2e68c66d781eb
theThirdTiwi <youdertubmail@gmail.com>
2023-11-22 19:04:36 +0100

    scaling ship ui

--- ship_ui.gd
+++ ship_ui.gd
@@ -1,29 +1,16 @@
 extends Node3D
 
+@export var healthObj : MeshInstance3D
+@export var armorObj : MeshInstance3D
+@export var shieldObj : MeshInstance3D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	#	hide ui
-	$Action.hide()
-	$Damage.hide()
 	add_to_group("ShipUI")
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-#	getter ans setter for the ship ui NO handling logic
-
-func setDamageDispaly():
-	pass
 
 # cALLS  setStats but gets the values from parent
 func updateShipUI():
 	var ship = get_parent()
 	setStats(ship.ship_name,ship.ship_current_health, ship.ship_health, ship.ship_current_armor, ship.ship_armor,ship.ship_current_shield, ship.ship_shield)
-
-	
 
 func setStats(name,health,maxHealth,armor,maxArmor,shield,maxShield):
 	setShipName(name)
@@ -33,23 +20,31 @@ func setStats(name,health,maxHealth,armor,maxArmor,shield,maxShield):
 	#print("set Stats")
 
 func setShipName(name):
-	#print("text name: ", name)
+	print("text name: ", name)
 	$Stats/Name.text = name
 	$Stats/Name.modulate = Color("gray")
 
 func setShipHealth(health,maxHealth):
+	healthObj.scale.z = getRelativeStats(health,maxHealth)
 	$Stats/Health.text = str(health) + " / " + str(maxHealth)
 	$Stats/Health.modulate = Color("red")
 	
 func setShipArmor(armor,maxArmor):
+	armorObj.scale.z = getRelativeStats(armor,maxArmor)
 	$Stats/Armor.text = str(armor) + " / " + str(maxArmor)
 	$Stats/Armor.modulate = Color("yellow") 
 
 func setShipShield(shield,maxShield):
+	shieldObj.scale.z = getRelativeStats(shield,maxShield)
 	$Stats/Shield.text = str(shield) + " / " + str(maxShield)
 	$Stats/Shield.modulate = Color("blue")
+
+func getRelativeStats(current, max)->float:
+	print("testtesttest Update SHIPUI ",current ," ",max)
+	if max > 0:
+		print("testtesttest Update SHIPUI ",current ," ",max, " ", current / float(max))
+		return float(current) / float(max)
+	return 0
+
+
 	
-
-
-func _on_attack_pressed():
-	pass # Replace with function body.
