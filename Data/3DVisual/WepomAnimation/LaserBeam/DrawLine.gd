eac3532bc884015664e613272b72b017d77fab1d
jannisdittmer <dittmer.jannis@yahoo.de>
2023-11-25 15:46:48 +0100

    modify LaserRayCode
    -modify LaserRayCode

--- Data/3DVisual/WepomAnimation/LaserBeam/DrawLine.gd
+++ Data/3DVisual/WepomAnimation/LaserBeam/DrawLine.gd
@@ -8,15 +8,19 @@ var position2: Vector3;
 @export var fireParticle: GPUParticles3D;
 @export var targetParticle: GPUParticles3D;
 
+func _ready():
+	hide()
+
 # Called when the node enters the scene tree for the first time.
 func fire(position1: Vector3,position2: Vector3, timeout : float = 1):
+	curve.clear_points()
 	curve.add_point(to_local(position1))
 	curve.add_point(to_local(position2))
-	fireParticle.position = to_local(position1)
-	targetParticle.position = to_local(position2)
-	pathFollow.progress_ratio = 0.5
+	fireParticle.set_position(to_local(position1))
+	targetParticle.set_position(to_local(position2))
+	pathFollow.set_progress_ratio(0.5)
 	show()
-	Capsule.scale = (Vector3(2, curve.get_baked_length()/2, 2))
+	Capsule.set_scale(Vector3(0.3, curve.get_baked_length()/2, 0.3))
 	await get_tree().create_timer(timeout).timeout
 	hide()
 	curve.clear_points()
