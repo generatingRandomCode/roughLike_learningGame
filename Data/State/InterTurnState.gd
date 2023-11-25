ae03f59c61c645dd1858ba32f52e3d8556b12b41
theThirdTiwi <youdertubmail@gmail.com>
2023-11-25 14:32:22 +0100

    add round timer

--- Data/State/InterTurnState.gd
+++ Data/State/InterTurnState.gd
@@ -1,8 +1,13 @@
 extends State
 
+var roundTimer : int = 0
+@export var turnLabel : RichTextLabel
 
+func _ready():
+	turnLabel.hide()
 #	called at the start of combat and at start of each round
 func enter(parameter := {}) -> void:
+	displayRoundTimer()
 	await updateShips()
 	get_parent().transition_to("PlayerTurnState",{})
 
@@ -13,3 +18,8 @@ func updateShips():
 	#	update the ship ui
 	get_tree().call_group("ShipUI", "updateShipUI")
 	#updateEnergy
+
+func displayRoundTimer():
+	roundTimer += 1
+	turnLabel.text = "Round: " + str(roundTimer)
+	turnLabel.show()
