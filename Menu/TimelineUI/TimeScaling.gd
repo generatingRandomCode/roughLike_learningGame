8da124e72ab3ee40ff3f28642c3165e7b600901a
jannisdittmer <dittmer.jannis@yahoo.de>
2023-11-29 20:31:10 +0100

    -time Scale added
    -time scale simple code
    -Ui for time added

---
+++ Menu/TimelineUI/TimeScaling.gd
@@ -0,0 +1,23 @@
+extends TextureRect
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_half_pressed():
+	Engine.time_scale = 0.5
+
+
+func _on_normal_pressed():
+	Engine.time_scale = 1
+
+
+func _on_double_pressed():
+	Engine.time_scale = 2
--- Menu/TraderUI/UIScale.gd
+++ Menu/TraderUI/UIScale.gd
@@ -12,10 +12,17 @@ func _process(delta):
 
 
 func _on_trader_ui_resized():
+	resize()
+
+
+
+func _on_timeline_ui_resized():
+	resize()
+	
+func resize():
 	var x : float = DisplayServer.window_get_size().x
 	var y : float = DisplayServer.window_get_size().y
 	if(x/1920 > y/1080):
 		self.scale = Vector2(y/1080,y/1080)
 	else:
 		self.scale = Vector2(x/1920,x/1920)
-
